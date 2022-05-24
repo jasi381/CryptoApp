b95caa63129e994da34b72d8909a803994fd047c
jasmeet singh <sjasmeet438@gmail.com>
2022-05-24 18:59:06 +0530

    half completed

---
+++ app/src/main/java/com/jasmeet/cryptoapp/fragment/adapter/TopLossGainPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.jasmeet.cryptoapp.fragment.adapter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.jasmeet.cryptoapp.fragment.TopLossGainFragment
+
+class TopLossGainPagerAdapter(fragment :Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        val fragment = TopLossGainFragment()
+        val bundle =Bundle()
+        bundle.putInt("position", position)
+        fragment.arguments = bundle
+        return fragment
+
+
+    }
+}
